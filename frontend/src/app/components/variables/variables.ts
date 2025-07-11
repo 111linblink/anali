815f91785e8e76b148391121d7303fea9b710213
elizabethgutierrez27 <146129308+elizabethgutierrez27@users.noreply.github.com>
2025-07-11 00:09:47 -0600

    Ok

--- frontend/src/app/components/variables/variables.ts
+++ frontend/src/app/components/variables/variables.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router'; // <-- Importa Router
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-variables',
