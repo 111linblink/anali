c6fdc74d5777fe822708048a3ff435531a06babe
elizabethgutierrez27 <146129308+elizabethgutierrez27@users.noreply.github.com>
2025-07-13 16:54:12 -0600

    yaa

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from collections import defaultdict
+from sklearn.cluster import KMeans
 from io import StringIO
 import pandas as pd
 import joblib
